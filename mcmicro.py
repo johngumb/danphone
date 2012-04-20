4ee015edf01bf6bac8569e5acf8b53b2e60d246c
john <john@gumb.org>
2012-04-20 23:46:03 +0100

    rename mcmicro.py (again...)

--- mcmicro.py
+++ mcmicro.py
@@ -1,9 +1,13 @@
 import ft232r
 import ShiftReg
+import MC145158
 
 class McMicro:
     def __init__(self):
         self.SR_POWER=0x40
+        self.SR_TX_RX=0x20 # pin 6
+
+        self.m_synth_refclk = 14.4E6
 
         return
 
@@ -12,6 +16,20 @@ class McMicro:
             self.m_shiftreg.setbit(self.SR_POWER)
         else:
             self.m_shiftreg.clearbit(self.SR_POWER)
+
+        self.m_shiftreg.latch()
+
+        return
+
+    def enable_tx(self):
+        self.m_shiftreg.setbit(self.SR_TX_RX)
+
+        self.m_shiftreg.latch()
+
+        return
+
+    def disable_tx(self):
+        self.m_shiftreg.clearbit(self.SR_TX_RX)
 
         self.m_shiftreg.latch()
 
@@ -30,6 +48,29 @@ class McMicro:
         self.m_shiftreg = ShiftReg.ShiftReg(self.m_hwif, self.m_hwif.D0, \
                                          self.m_hwif.D1, self.m_hwif.D3, 8)
 
+        #
+        # synth
+        # prescaler is MC14094 (divide by 40)
+        #
+        self.m_synth = MC145158.MC145158(self.m_hwif, 40, self.m_hwif.D1, \
+                                         self.m_hwif.D0, self.m_hwif.D2)
+
+        return
+
+    def tune(self):
+        #
+        # 14.4 Mhz reference clock
+        #
+        self.m_synth.set_refclk(self.m_synth_refclk)
+
+        #
+        # nail up 6.25 Khz steps for now
+        #
+        self.m_synth.set_ref_divider(self.m_synth_refclk/6.25E3)
+
+        # 104.88726E rx == 74.1 MHz TX approx
+        self.m_synth.set_freq(104.88726E6)
+
         return
 
 def test():
@@ -38,6 +79,10 @@ def test():
     mc.initialise()
 
     mc.power(True)
+
+    mc.tune()
+
+    mc.enable_tx()
 
 if __name__ == "__main__":
     
