3cefc5953befddc307f36f9f2d2a83dbbfaa55c2
John Gumb <jag@jgumbt61.(none)>
2012-08-04 01:25:21 +0100

    most shift register bits sorted

--- mcmicro.py
+++ mcmicro.py
@@ -2,10 +2,17 @@ import ft232r
 import ShiftReg
 import MC145158
 
+# could leave tx unlocked to prevent TX PA enable
 class McMicro:
     def __init__(self):
+        self.SR_AUDIO_PA=0x80
         self.SR_POWER=0x40 # pin 5
-        self.SR_TX_RX=0x20 # pin 6
+        self.SR_TX_RX=0x20 # pin 6, ensure PA stays off initially
+        self.SR_TX_POWER_HI_LO=0x10
+        self.SR_TX_PA=0x08
+        self.SR_UNUSED=0x04
+        self.SR_TX_MUTE=0x02
+        self.SR_AUDIO_ENABLE=0x01
 
         #0x80 pin 4
         #0x10 pin 7 hi/lo power ??
@@ -39,8 +46,13 @@ class McMicro:
         return
 
     def enable_pa(self):
+#        self.SR_PA=0x02 # enable tx audio
+        self.SR_PA=0x04
+#        self.SR_PA=0x00
+#        self.SR_PA=0x01
+#        self.m_shiftreg.setbit(self.SR_PA)
         self.m_shiftreg.setbit(self.SR_PA)
-#        self.m_shiftreg.clearbit(self.SR_PA)
+
 
         self.m_shiftreg.latch()
 
@@ -75,6 +87,13 @@ class McMicro:
 
         return
 
+    def enable_audio(self):
+        self.m_shiftreg.setbit(self.SR_AUDIO_ENABLE)
+
+        self.m_shiftreg.latch()
+
+        return
+        
     def tune(self):
         #
         # 14.4 Mhz reference clock
@@ -89,6 +108,8 @@ class McMicro:
         # 104.88726E rx == 74.1 MHz TX approx
         #self.m_synth.set_freq(104.88726E6)
         self.m_synth.set_freq(70.38750E6+21.4E6)
+        #self.m_synth.set_freq(70.18E6+21.4E6)
+        #self.m_synth.set_freq(70.4875E6+21.4E6)
         #self.m_synth.set_freq(70.3875E6+21.4E6)
         #self.m_synth.set_freq(70.050E6+21.4E6)
         #self.m_synth.set_freq(65.38750E6+21.4E6)
@@ -107,8 +128,10 @@ def test():
     mc.power(True)
     #mc.power(False)
 
+    mc.enable_audio()
 
     mc.tune()
+
 
 #    mc.disable_tx()
 #    mc.enable_tx()
