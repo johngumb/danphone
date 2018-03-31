7f1f83f13961065e3340fcb29977e3b2484189ae
john <john@gumb.org>
2018-04-01 00:15:07 +0100

    add waitpid

--- utils/cmdseq.sh
+++ utils/cmdseq.sh
@@ -8,12 +8,12 @@ if [ ! -p ${CMDFIFO} ]; then
    mkfifo ${CMDFIFO}
 fi
 
-let maxattempts=10
-
 execute_cmd()
 {
     CMD="$1"
     let attempts=0
+    let maxattempts=10
+
     while [ ${attempts} -lt ${maxattempts} ]; do
         if ${CMD}; then
             break
@@ -23,24 +23,40 @@ execute_cmd()
     done
 }
 
+waitpid()
+{
+    while kill -0 $1; do
+        sleep 0.1
+    done
+}
+
 while true; do
       if read -n ${MAXCHAR} FULLCMD < ${CMDFIFO}; then
           fromnode=$(echo ${FULLCMD} | awk '{print $1}')
-          CMD=$(echo ${FULLCMD} | awk '{$1= ""; print $0}')
+          CMD=$(echo ${FULLCMD} | awk '{$1=""; print $0}')
           first=$(echo ${CMD} | awk '{print $1}')
+          pernode_fifo="/tmp/${fromnode}_recfifo"
 
           if [ "${first}" == "jack_capture" ]; then
-              pernode_fifo="/tmp/${fromnode}_recfifo"
               ${CMD} &
               echo $! > ${pernode_fifo}
           elif [ "${first}" == "kill" ]; then
-              pernode_fifo="/tmp/${fromnode}_recfifo"
-              pid=$(echo ${CMD} | awk '{print $2}')
+              pid=$(echo ${CMD} | awk '{print $NF}')
               execute_cmd "${CMD}"
+
+              # need to wait for process to die
+              waitpid ${pid}
+
+              # let requesting process know recording has stopped
               echo ${pid} > ${pernode_fifo}
           else
               execute_cmd "${CMD}"
           fi
       fi
+      readstat=$?
+      if [ ${readstat} -ne 0 ]; then
+          echo "$0: read failed"
+          sleep 0.1
+      done
 done
 
