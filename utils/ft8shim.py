c078238b924ee8ebd3a6af6a2731071c8ed43973
John Gumb <john@gumb.org>
2018-12-05 01:03:39 +0000

    beginnings of wsjtx shim

---
+++ utils/ft8shim.py
@@ -0,0 +1,10 @@
+class WsjtxListener
+    def __init__(self, socketname_for_wsjtx):
+        
+class RefOsc:
+    def __init__(self, datafile, basefreq):
+
+    def freq_to_dac(self, freq):
+
+class RadioCmdSender(self):
+    def __init__(self):
