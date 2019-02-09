8540c64d33b6e50211db212f24232a9ea2d47cdf
John Gumb <john@gumb.org>
2019-02-09 00:59:33 +0000

    add 16 bit DAC to 4 metre micro

--- McMicro.py
+++ McMicro.py
@@ -547,8 +547,18 @@ class McMicro:
             if not self.m_ftdi and self.m_hwif.server()=="rudd":
                 # 14.4MHz on ref osc
 
-                val=0xC3B8
+                val=0xC370
                 calfile="/home/john/2mcal"
+                if os.path.exists(calfile):
+                    with open(calfile) as caldata:
+                        val+=int(caldata.read())
+                self.m_hwif.enqueue("M%04X" % val)
+
+            if not self.m_ftdi and self.m_hwif.server()=="dab":
+                # 14.4MHz on ref osc
+
+                val=0xC370
+                calfile="/home/john/4mcal"
                 if os.path.exists(calfile):
                     with open(calfile) as caldata:
                         val+=int(caldata.read())
