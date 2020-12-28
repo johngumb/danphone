267dd9a2484a95dbd2bc2850bbfef805c43e7968
John Gumb <john@gumb.org>
2020-12-28 01:33:32 +0000

    more mod stuff working

--- utils/ics307gi03/ics307gi03.erl
+++ utils/ics307gi03/ics307gi03.erl
@@ -26,6 +26,12 @@ bin_to_hex(Bin) when is_binary(Bin) ->
       erlang:integer_to_list(LastNibble2, 2),
       " >>" ].
 
+hex(C) when C < 10 -> $0 + C;
+hex(C) -> $a + C - 10.
+
+hexlify(Bin) when is_binary(Bin) ->
+    << <<(hex(H)),(hex(L))>> || <<H:4,L:4>> <= Bin >>.
+
 % Table 2. VCO Divider
 vco_divider(<<_:108, VcoDivider:11, _:13>>) ->
     % VcoDivider is bound to a little endian 11 bit integer in the match.
@@ -149,16 +155,135 @@ o2_div(<<_:14, V:4, L:1, _:113>>)->
 o3_div(<<_:10, V:4, _:24, L:1, _:93>>)->
     calc_O2O3(V, L).
 
+o2div_return_or_continue(_W, NW, ReqDiv, ReqDiv, V)->
+    io:format("PPPP ~p ~p ~n",[ReqDiv, V]),
+    NW;
+
+o2div_return_or_continue(_W, _NW, _, _, 32) ->
+    notfound;
+
+o2div_return_or_continue(W, _NW, ReqDiv, ActDiv, V)->
+    mod_o2div_worker(W, ReqDiv, V+1).
+
+mod_o2div_worker(W, ReqDiv, V)->
+    <<H:14, _:5, L:113>> = W,
+    ActDiv = o2_div(NW= <<H:14, V:5, L:113>>),
+    o2div_return_or_continue(W, NW, ReqDiv, ActDiv, V).
+
+mod_o2div(W, ReqDiv)->
+    mod_o2div_worker(W, ReqDiv, 0).
+
+o3div_return_or_continue(_W, NW, ReqDiv, ReqDiv, V, F)->
+    NW;
+
+o3div_return_or_continue(_W, _NW, _, _, 32, F) ->
+    notfound;
+
+o3div_return_or_continue(W, _NW, ReqDiv, ActDiv, V, F)->
+    %mod_o3div_worker(W, ReqDiv, V+1).
+    F(W, ReqDiv, V+1).
+
+mod_o3div_worker(W, ReqDiv, V)->
+    <<H:10, _:4, M:24, _:1, L:93>> = W,
+    <<_:3, VU:4,VL:1>> = <<V>>,
+    io:format("~p~n",[V]),
+    ActDiv = o3_div(NW= <<H:10, VU:4, M:24, VL:1, L:93>>),
+    o3div_return_or_continue(W, NW, ReqDiv, ActDiv, V, fun mod_o3div_worker/3).
+
+mod_o3div(W, ReqDiv)->
+    mod_o3div_worker(W, ReqDiv, 0).
+
 % Table 8 Misc Control Bits
-misc_bits(<<0:2, OE3:1, MBZ:2, _:2, Clk3Src:1, Clk2Src:1, XtalInput:1, _:10, P:1, OE2:1, OE1:1, _:109>>)->
+report_misc_bits(<<0:2, OE3:1, _ICP:2, 0:2, Clk3Src:1, Clk2Src:1, XtalInput:1, _ODiv:9, P:1, OE2:1, OE1:1, _:110>>)->
     io:format("Clocks 1, 2, 3 enable: ~p ~p ~p ~n",[OE1, OE2, OE3]),
     io:format("XtalInput: ~p~n",[XtalInput]),
     io:format("Clk2Src Clk3Src: ~p ~p~n", [Clk2Src, Clk3Src]),
-    io:format("Powerdown ~p~n",[P]),
-    io:format("MBZ ~p~n",[MBZ]).
+    io:format("Powerdown ~p~n",[P]).
 
-mod_misc_bits(<<0:2, _OE3:1, MBZ:2, A:2, Clk3Src:1, Clk2Src:1, XtalInput:1, B:10, P:1, _OE2:1, _OE1:1, L:109>>, NOE1, NOE2, NOE3)->
-    <<0:2, NOE3:1, MBZ:2, A:2, Clk3Src:1, Clk2Src:1, XtalInput:1, B:10, P:1, NOE2:1, NOE1:1, L:109>>.
+mod_misc_bits(<<0:2, _OE3:1, ICP:2, 0:2, Clk3Src:1, Clk2Src:1, XtalInput:1, ODiv:9, P:1, _OE2:1, _OE1:1, L:110>>, NOE1, NOE2, NOE3)->
+    <<0:2, NOE3:1, ICP:2, 0:2, Clk3Src:1, Clk2Src:1, XtalInput:1, ODiv:9, P:1, NOE2:1, NOE1:1, L:110>>.
+
+report_input_divider(W)->
+    InputDivider = input_divider:input_divider(W),
+    io:format("Input Divider ~p~n",[InputDivider]),    
+    InputDivider.
+
+report_vco_divider(W)->
+    VcoDivider = vco_divider(W),
+    io:format("VCO Divider ~p~n",[VcoDivider]),
+    VcoDivider.
+
+report_charge_pump_current(W)->
+    CP_current=charge_pump_current(W),
+    io:format("Charge pump current uA ~p~n",[CP_current]),
+    CP_current.
+
+report_loop_filter_resistor(W)->
+    LFR=loop_filter_resistor(W),
+    io:format("Loop filter resistor ~pk ~n",[LFR]),
+    LFR.
+
+report_clk1_output_divider(W)->
+    OutputDivider1=o1_div(W),
+    io:format("CLK1 Output Divider ~p~n",[OutputDivider1]),
+    OutputDivider1.
+
+report_clk2_output_divider(W)->
+    OutputDivider2=o2_div(W),
+    io:format("CLK2 Output Divider ~p~n",[OutputDivider2]),
+    OutputDivider2.
+
+report_clk3_output_divider(W)->
+    OutputDivider3=o3_div(W),
+    io:format("CLK3 Output Divider ~p~n",[OutputDivider3]),
+    OutputDivider3.
+
+fmt_word_worker(<<>>)->
+    io:format("~n"),
+    ok;
+
+fmt_word_worker(<<B,Rest/bitstring>>)->
+    io:format("0x~2.16.0B, ",[B]),
+    fmt_word_worker(Rest).
+
+fmt_word(W)->
+    <<Wi:132>> =W,
+    PaddedW= <<0:4,Wi:132>>,
+    fmt_word_worker(PaddedW).
+
+report_word(W)->
+    <<Wi:132>>=W,
+    %io:format("~p~n",[W]),
+    io:format("+++++ Progword ~.16B ++++~n", [Wi]),
+
+    fmt_word(W),
+
+    InputDivider=report_input_divider(W),
+    VcoDivider=report_vco_divider(W),
+    OutputDivider1 = report_clk1_output_divider(W),
+    OutputDivider2 = report_clk2_output_divider(W),
+    OutputDivider3 = report_clk3_output_divider(W),
+
+    InputFreq = 10, % 10MHz
+
+    VCOFreq = InputFreq * VcoDivider/InputDivider,
+    Clk1Freq = VCOFreq/OutputDivider1,
+    Clk2Freq = VCOFreq/OutputDivider2,
+    Clk3Freq = VCOFreq/OutputDivider3,
+
+    io:format("VCO Freq ~p~n",[VCOFreq]),
+    io:format("CLK1 Freq ~p~n",[Clk1Freq]),
+    io:format("CLK2 Freq ~p~n",[Clk2Freq]),
+    io:format("CLK3 Freq ~p~n",[Clk3Freq]),
+
+    ok = ?assert(VCOFreq >= 100.0),
+    ok = ?assert(VCOFreq =< 730.0),
+
+    report_loop_filter_resistor(W),
+    report_charge_pump_current(W),
+    report_misc_bits(W),
+    io:format("----- Progword ~.16B ----~n~n", [Wi])
+.
 
 main() ->
     
@@ -173,79 +298,44 @@ main() ->
 
     OrigWord = <<Progword:132>>,
 
-    io:format("~p~n",[OrigWord]),
+    report_word(OrigWord),
+
+    %% io:format("~p~n",[OrigWord]),
 
 
-    InputDivider = input_divider:input_divider(OrigWord),
-%    VcoDivider = vco_divider(OrigWord),
-
-    io:format("Input Divider ~p~n",[InputDivider]),
-
-    WW1=mod_vco_divider(OrigWord, 232),
-    Word = WW1,
-    io:format("~p~n",[Word]),
-    VcoDivider = vco_divider(Word),
-    io:format("VCO Divider ~p~n",[VcoDivider]),
-
-    CP_current=charge_pump_current(Word),
-    io:format("Charge pump current uA ~p~n",[CP_current]),
-
-    LFR=loop_filter_resistor(Word),
-    io:format("Loop filter resistor ~pk ~n",[LFR]),
-
-    W2=mod_loop_filter_resistor(Word,2),
-    LFR2=loop_filter_resistor(W2),
-    io:format("Loop filter resistor after mod ~pk ~n",[LFR2]),
-
-    % report modified word
-    <<W2i:132/integer-unsigned-big>>=W2,
-    io:format("~.16B~n",[W2i]),
-
-    NewCpi=26.25,
-    W3=mod_cpi(Word, NewCpi),
-    io:format("modded cpi for ~p: ~p ~n",[NewCpi, W3]),
-    <<W3i:132/integer-unsigned-big>>=W3,
-    io:format("~.16B~n",[W3i]),
-
-    misc_bits(Word),
-    XW=mod_misc_bits(Word,1,1,0),
-    misc_bits(XW),
 
 
-    NV=20,
-    C1W=mod_o1div(Word, NV),
-    io:format("CLK1 Output Divider modded ~p ~p ~n",[NV, C1W]),
+    %% WW1=mod_vco_divider(OrigWord, 232),
+    %% W2=mod_loop_filter_resistor(Word,2),
 
-    Clk1_output_div=o1_div(C1W),
-    io:format("CLK1 Output Divider ~p~n",[Clk1_output_div]),
+    %% % report modified word
+    %% <<W2i:132/integer-unsigned-big>>=W2,
+    %% io:format("~.16B~n",[W2i]),
 
-    OutputDivider2=o2_div(C1W),
-    io:format("CLK2 Output Divider ~p~n",[OutputDivider2]),
+    %% NewCpi=26.25,
+    %% W3=mod_cpi(Word, NewCpi),
+    %% io:format("modded cpi for ~p: ~p ~n",[NewCpi, W3]),
+    %% <<W3i:132/integer-unsigned-big>>=W3,
+    %% io:format("~.16B~n",[W3i]),
 
-    OutputDivider3=o3_div(C1W),
-    io:format("CLK3 Output Divider ~p~n",[OutputDivider3]),
-
-    OutputDivider1=Clk1_output_div,
-    InputFreq = 10, % 10MHz
-
-    VCOFreq = InputFreq * VcoDivider/InputDivider,
-    Clk1Freq = VCOFreq/OutputDivider1,
-    Clk2Freq = VCOFreq/OutputDivider2,
-    Clk3Freq = VCOFreq/OutputDivider3,
-
-    io:format("VCO Freq ~p~n",[VCOFreq]),
-    io:format("CLK1 Freq ~p~n",[Clk1Freq]),
-    io:format("CLK2 Freq ~p~n",[Clk2Freq]),
-    io:format("CLK3 Freq ~p~n",[Clk3Freq]),
-
-    <<C1Wi:132>>=W2,
-    io:format("~.16B~n",[C1Wi]),
-
-    ok = ?assert(VCOFreq>100.0),
-    ok = ?assert(VCOFreq<730.0)
+    %% misc_bits(Word),
+    XW=mod_misc_bits(OrigWord,1,1,0),
+    report_word(XW),
+    
 
 
-    %ad9862:test(1 + 128 + 256 + 512)
+    %% NV=20,
+    C1W=mod_o1div(XW, 20),
+    report_word(C1W),
+    %% io:format("CLK1 Output Divider modded ~p ~p ~n",[NV, C1W]),
+
+
+    C3W=mod_o3div(C1W, 4),
+    report_word(C3W)
+
+
+
+    %% %ad9862:test(1 + 128 + 256 + 512)
 .
 
 
