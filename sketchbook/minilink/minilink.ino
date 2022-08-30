8b1d510bfd044f8551a454c2ee862914065573ea
John Gumb <john@gumb.org>
2022-08-31 00:10:47 +0100

    ericsson minilink

--- sketchbook/minilink/minilink.ino
+++ sketchbook/minilink/minilink.ino
@@ -5,14 +5,12 @@
 
 void setup() {
   // put your setup code here, to run once:
-#if 0
   Serial.begin(115200);
   Serial.println("Minilink");
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
   SPI.setClockDivider(SPI_CLOCK_DIV64);
-#endif
 
   pinMode(LATCH, OUTPUT);
   pinMode(DATA, OUTPUT);
@@ -20,14 +18,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // pin 11 red
+  // pin 11 red wire; data
 
-  digitalWrite(DATA, LOW);
-
-  digitalWrite(LATCH, HIGH);
-  delay(2000);
-  digitalWrite(LATCH, LOW);
-  delay(2000);
-
-
+  Serial.println("writing...");
+  SPI.transfer(0x01);
+  delay(1000);
 }
